793c3dcd5517acdb676fed365438ee04d85d3253
edilara <ercandilara0@gmail.com>
2023-02-01 10:32:36 +0100

    updated initxml.py with main function

--- initxml.py
+++ initxml.py
@@ -1,7 +1,7 @@
-import pandas as pd
 import xml.etree.ElementTree as et
 from copy import deepcopy
 import re
+from prepdata import importExcel, getLength
 
 def getXML(path = 'habides-derogations-instance.xml'):
     return et.parse(path)
@@ -13,4 +13,30 @@ def getCopyEntry(root, entry = 'derogation'):
     derogation = root.find(entry)
     return deepcopy(derogation)
 
+def duplicateElement(root, lenDataDerogations, copy):
+    i = 1
+    while i != lenDataDerogations:
+        i += 1
+        root.append(copy)
+    return root
 
+def toString(tree):
+    return et.tostring(tree.getroot(), encoding = 'utf-8')
+
+def writeToXMLFile(tree, filename = 'emptyXML.xml'):
+    tree.write(filename, encoding = 'UTF-8', xml_declaration = True) 
+
+def main():
+    dataDerogations = importExcel('testfile.xlsx')
+    lenDataDerogations = getLength(dataDerogations)
+    tree = getXML('habides-derogations-instance.xml')
+    root = getRootXML(tree)
+    copy = getCopyEntry(root, 'derogation')
+    root = duplicateElement(root, lenDataDerogations, copy)
+
+    writeToXMLFile(tree = tree, filename = 'empty.xml')
+    
+    
+if __name__ == '__main__':
+    main()
+    
