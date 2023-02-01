bdd12c92cc2b33096ed4acb2bec459e3622835c8
edilara <ercandilara0@gmail.com>
2023-02-01 10:47:31 +0100

    added draft xml validation functions

---
+++ validatexml.py
@@ -0,0 +1,11 @@
+import xmlschema
+from initxml import getXML
+
+def load_xsd(path = 'derogations.xsd'):
+    return xmlschema.XMLSchema(path)
+
+def validate(xsd, xml = 'empty.xml'): 
+    return xmlschema.validate(xml, xsd)
+
+def main():
+    return validate(xsd = load_xsd(), xml = validate(getXML()))
