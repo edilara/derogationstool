369f07de5c1477101d348a2b7c181edd6b9dc494
edilara <ercandilara0@gmail.com>
2023-02-21 19:08:48 +0100

    added new functions to fillxml.py

--- src/fillxml.py
+++ src/fillxml.py
@@ -11,7 +11,6 @@ def setDerogations(root, userIdentity = 'TestGovt', country = 'XX'):
     root.set('country', country)
     return root
 
-#in the tags
 def setDirective(root, directive = 'http://rod.eionet.europa.eu/obligations/268'):
     for entry in root.findall('derogation'):
         entry.set('directive', directive)
@@ -21,3 +20,29 @@ def setStatus(root, status = 'complete'):
     for entry in root.findall('derogation'):
         entry.set('status', status)
     return root 
+
+def setDirectiveDerogation(derogation, directive = 'http://rod.eionet.europa.eu/obligations/268'):
+    return derogation.set('directive', directive)
+
+def setStatusDerogation(derogation, status = 'complete'):
+    return derogation.set('status', status)
+
+def fillAttribKeys(dictkey, dict, element):
+    return element.set(dictkey, str(dict[dictkey]))
+
+def fillFirstLevel(dictkey, dict, element):
+    for elem in element.findall(dictkey):
+        elem.text = str(dict[dictkey])
+        return element 
+
+def fillSecondlevelPrefix(prefixlist, dictkey, dict, element):
+    for n in prefixlist:
+        if dictkey == n:
+            for elem in element.findall(n.split('_')[0]):
+                for nelem in elem.findall(n.split('_')[1]):
+                    nelem.text = str(dict[n])
+        else:
+            continue
+    
+    return element
+
