30198be0d17c245c1c38903b26afb9b387ff7501
edilara <ercandilara0@gmail.com>
2023-02-21 19:07:04 +0100

    deleted unused imports

--- src/initxml.py
+++ src/initxml.py
@@ -2,9 +2,14 @@ import xml.etree.ElementTree as et
 from copy import deepcopy
 import re
 from prepdata import importExcel, getLength
+from requests import get 
 
-def getXML(path = 'habides-derogations-instance.xml'):
-    return et.parse(path)
+def getXML(pathxml = 'habides-derogations-instance.xml'):
+    return et.parse(pathxml)
+
+def retrieveInstance(urlpath = 'http://dd.eionet.europa.eu/schemas/habides-2.0/habides-derogations-instance.xml'):
+    response = get(urlpath)
+    return et.fromstring(response.content)
 
 def getRootXML(tree):
     return tree.getroot()
@@ -13,30 +18,15 @@ def getCopyEntry(root, entry = 'derogation'):
     derogation = root.find(entry)
     return deepcopy(derogation)
 
-def duplicateElement(root, lenDataDerogations, copy):
+def appendElement(root, lenDataDerogations, copy):
     i = 1
     while i != lenDataDerogations:
         i += 1
         root.append(copy)
     return root
 
-def toString(tree):
-    return et.tostring(tree.getroot(), encoding = 'utf-8')
 
 def writeToXMLFile(tree, filename = 'empty.xml'):
     tree.write(filename, encoding = 'UTF-8', xml_declaration = True) 
 
-def main():
-    dataDerogations = importExcel('testfile.xlsx')
-    lenDataDerogations = getLength(dataDerogations)
-    tree = getXML('habides-derogations-instance.xml')
-    root = getRootXML(tree)
-    copy = getCopyEntry(root, 'derogation')
-    root = duplicateElement(root, lenDataDerogations, copy)
-
-    writeToXMLFile(tree = tree)
-    
-    
-if __name__ == '__main__':
-    main()
     
