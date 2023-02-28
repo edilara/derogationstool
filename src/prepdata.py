7a3ae70a609dc575517250e8977c30465069147b
edilara <ercandilara0@gmail.com>
2023-02-28 10:02:22 +0100

    added enforceDates

--- src/prepdata.py
+++ src/prepdata.py
@@ -1,22 +1,35 @@
 import pandas as pd
 import json 
 
-def importExcel(path):
-    return pd.read_excel(path, keep_default_na= False)
+def importExcel(pathdata):
+    return pd.read_excel(pathdata, keep_default_na= False)
 
 def replaceTrueFalse(df):
     return df.replace('No', 'false').replace('Yes', 'true')
 
-def getLength(dataDerogations):
-    return len(dataDerogations)
+def addDummyCols(df):
+    df['licensed'] = ''
+    df['actuallyTaken'] = ''
+    return df
 
 def loadTranslations(path = 'translations.json'):
-    with open('translations.json', 'r+') as tr:
+    with open(path, 'r+') as tr:
         transdict= json.loads(tr.read())
     return transdict
 
 def transformCols(df, transdict):
     return df.rename(transdict, axis=1, inplace=True)
+
+def enforceDates(df):
+    for col in df:
+        if col.dtype == 'datetime64[ns]':
+            df[col] = pd.to_datetime(df[col].dt.strftime('%d-%m-%y'))
+        else:
+            continue
+    return df
+
+def dfToDict(df):
+    return df.fillna('').to_dict(orient = 'records')
 
 def getTags(root):
     taglist = []
@@ -24,3 +37,4 @@ def getTags(root):
         for x in d:
             taglist.append(x.tag)
     return taglist
+
